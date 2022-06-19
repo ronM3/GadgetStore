925d05eec18b8bb4a93de8c1a7a032cad70c2208
Ron <ron00003@gmail.com>
2022-06-19 22:01:03 +0300

    fetch product through redux

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,29 +1,30 @@
 import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import Product from '../components/Product'
 import { Row, Col } from 'react-bootstrap'
+import { listProducts } from '../actions/productAction'
 import axios from 'axios'
 const HomePage = () => {
-  
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  // const [products, setProducts] = useState([]);
+  const productList = useSelector(state => state.productList)
+  const { loading, error, products } = productList
 
   useEffect(()=>{
-    const fetchProducts = async () =>{
-      const { data } = await axios.get('/api/products')
-      setProducts(data)
-    }
-    fetchProducts()
-  }, [])
+    dispatch(listProducts())
+  }, [dispatch])
 
   return (
     <>
         <h1>Latest Products</h1>
-        <Row>
+        {loading ? <h2>Loading...</h2> : error ? <h3>{error}</h3> :       <Row>
             {products.map(product => (
                 <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                     <Product product={product}/>
                 </Col>
             ))}
-        </Row>
+        </Row>}
+  
     </>
   )
 }
